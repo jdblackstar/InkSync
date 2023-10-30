d4a2b8619f6b6a87135e120719f8679d9c268840
Josh Black-Star <23347017+jdblackstar@users.noreply.github.com>
2023-10-30 14:13:31 -0700

    register ScrapeInfo model

--- Kindle/admin.py
+++ Kindle/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Highlight, Tag
+from .models import Highlight, Tag, ScrapeInfo
 
 # registering both the Highlight and Tag models here
 admin.site.register(Highlight)
 admin.site.register(Tag)
+admin.site.register(ScrapeInfo)
